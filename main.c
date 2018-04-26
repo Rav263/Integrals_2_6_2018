8229a558dbc0637ec3708981f4b48c42600e5e2c
RAV263 <nickiforov.nik@gmail.com>
2018-04-26 12:08:52 +0300

    Разбор вызова ассемблерных функций из под си

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+//#include "functions.asm"
+
+double f1(double);
+double f2(double);
+double f3(double);
+
 int main(void){
+  double a;
+
+  scanf("%lf", &a);
+
+  double b = f1(a);
+  double c = f2(a);
+  double d = f3(a);
+
+  printf("%lf %lf %lf\n", b, c, d);
+  
   return 0;
 }
